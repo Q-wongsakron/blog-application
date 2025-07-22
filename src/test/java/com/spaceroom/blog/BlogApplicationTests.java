507ad234eac9023d472fa72c0fbd77c39f0a8934
wongsakron kongkamud <wongsakronkongkamud@gmail.com>
2025-07-22 14:18:53 +0700

    initial commit

---
+++ src/test/java/com/spaceroom/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceroom.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
